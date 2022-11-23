6fe5b2213cd905353695c0ff49eb772b9a218f4d
Pascal Lubbers <pascal.lubbers@dyflexis.com>
2022-11-23 14:12:24 +0100

    Build the docker image if it does not yet exist when running tests

--- test.sh
+++ test.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env sh
 set -e
 
-docker run --rm dyflexis-oss/build-your-own-programming-language:0.1.0 vendor/bin/phpunit
+IMAGE=dyflexis-oss/build-your-own-programming-language:0.1.0
+
+if [ "$(docker images -q ${IMAGE} 2> /dev/null)" = "" ]
+then
+  ./build.sh
+fi
+
+docker run --rm ${IMAGE} vendor/bin/phpunit
